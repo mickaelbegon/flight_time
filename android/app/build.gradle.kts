b09aad190d7a448a7c82473e4704423c18c4b70b
mickaelbegon <mickaelbegon@gmail.com>
2026-09-24 16:45:43 -0400

    Use supported Kotlin version in Android CI

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -3,7 +3,8 @@ import java.io.FileInputStream
 
 plugins {
     id("com.android.application")
-    // The Flutter Gradle Plugin supplies Built-in Kotlin through AGP.
+    id("org.jetbrains.kotlin.android")
+    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
 }
 
@@ -21,6 +22,10 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_17.toString()
     }
 
     defaultConfig {
